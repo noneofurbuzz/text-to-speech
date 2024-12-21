670402d933842ebb876aac7411b21ade6b022661
Ugochi Ike <ugochiike26@gmail.com>
2024-12-21 23:31:12 +0100

    feat: add controls for pause and play

--- src/components/MusicPlayer.tsx
+++ src/components/MusicPlayer.tsx
@@ -1,18 +1,30 @@
-import { useContext } from "react"
+import { useContext, useEffect, useState } from "react"
 import { TextContext } from "../context/textContext"
 
 export function Player(){
     const {value} = useContext(TextContext)
+    const [pause,setPause] = useState(false)
     let text = new SpeechSynthesisUtterance(value.text)
     function playVoice(){
-        console.log("hi")
-        console.log(speechSynthesis.getVoices())
+        if (!speechSynthesis.speaking){
+            console.log(speechSynthesis.getVoices())
         speechSynthesis.speak(text)
+        }
+        setPause(prev => !prev)
+        if (pause === true){
+            speechSynthesis.pause()
+        }
+        else{
+            speechSynthesis.resume()
+        }
+        text.addEventListener("end",(event) => {
+            setPause(false)
+        })
     }
     return(
         <button onClick={playVoice} className="bg-black fixed bottom-8 items-center w-48 max-w-full flex justify-center py-4 rounded-full">
-            <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="#ffffff" stroke="#ffffff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className=""><polygon points="6 3 20 12 6 21 6 3"/></svg>
-            <span className="text-white ml-3 font-outfit">Play</span>
+            {!pause ? <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="#ffffff" stroke="#ffffff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><polygon points="6 3 20 12 6 21 6 3"/></svg> : <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="#ffffff" stroke="#ffffff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><rect x="14" y="4" width="4" height="16" rx="1"/><rect x="6" y="4" width="4" height="16" rx="1"/></svg>}
+            <span className="text-white ml-3 font-outfit">{pause ? 'Pause' : 'Play'}</span>
         </button>
     )
 }
