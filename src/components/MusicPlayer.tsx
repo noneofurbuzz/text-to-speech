15cf6b4f25b730f855a6305b917e8b1f7cd36933
Ugochi Ike <ugochiike26@gmail.com>
2024-12-21 15:11:35 +0100

    feat: Add play button

---
+++ src/components/MusicPlayer.tsx
@@ -0,0 +1,8 @@
+export function PLayer(){
+    return(
+        <button className="bg-black fixed bottom-8 items-center w-48 max-w-full flex justify-center py-4 rounded-full">
+            <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="#ffffff" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className=""><polygon points="6 3 20 12 6 21 6 3"/></svg>
+            <span className="text-white ml-3 font-outfit">Play</span>
+        </button>
+    )
+}
--- src/pages/Text.tsx
+++ src/pages/Text.tsx
@@ -1,3 +1,4 @@
+import { PLayer } from "../components/MusicPlayer"
 import { Navbar } from "../components/Navbar"
 
 export function Text(){
@@ -5,8 +6,9 @@ export function Text(){
         <section className="bg-[#F1F4F9] flex justify-center ">
         <Navbar />
             <form className="mt-12 h-screen w-[55rem] font-sabon">
-                <textarea className="h-full pt-16 px-12 outline-none w-full"  placeholder="Type or Paste Text"></textarea>
+                <textarea className="h-full pt-16 px-12 outline-none resize-none w-full text-xl"  placeholder="Type or Paste Text"></textarea>
             </form>
+            <PLayer />
         </section>
         
     )
