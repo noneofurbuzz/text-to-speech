fea8021e0644ae5e2555fd013f44765e2d56e6d5
Ugochi Ike <ugochiike26@gmail.com>
2024-12-22 12:36:18 +0100

    feat: cancel speech utterance whenback button pressed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,9 +2,14 @@ import { useNavigate } from "react-router-dom";
 
 export function Navbar(){
     const navigate = useNavigate()
+
+    function handleClick(){
+        navigate('/')
+        speechSynthesis.cancel()
+    }
     return(
         <nav className="border-b-2 border-[#E4EAF1] py-4 text-xl left-0 bg-white right-0 fixed ">
-           <button onClick={() => navigate('/')} className="flex font-bold justify-center items-center gap-0.5 border-[#ECECF0] border-[1px] py-2 ml-3 font-outfit tracking-widest text-base  px-2"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-arrow-left"><path d="m12 19-7-7 7-7"/><path d="M19 12H5"/></svg><span>Go back</span></button>
+           <button onClick={handleClick} className="flex font-bold justify-center items-center gap-0.5 border-[#ECECF0] border-[1px] py-2 ml-3 font-outfit tracking-widest text-base  px-2"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-arrow-left"><path d="m12 19-7-7 7-7"/><path d="M19 12H5"/></svg><span>Go back</span></button>
         </nav>
     )
 }
