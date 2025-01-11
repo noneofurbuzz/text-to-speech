b6315775703d9dd9c3b6c936e62d3eb27fa11041
Ugochi Ike <ugochiike26@gmail.com>
2025-01-11 15:19:50 +0100

    refactor: ensure only pdf and txt are specified as accepted files

--- src/components/UploadPdf.tsx
+++ src/components/UploadPdf.tsx
@@ -65,7 +65,7 @@ export function Uploadpdf({notify} : {notify : (message: string) => void}){
         </div>
             <label onDrop={handleDrop} onDragOver={handleDragOver}   htmlFor="upload" className={`${dragOver? 'bg-[#F2F7FF] border-[#7ACAF3]' : 'bg-white border-[#D7DEE8]'} 2cursor-pointer mt-8 border-dashed py-20 flex rounded-md flex-col justify-center items-center border-2 `}><svg className="fill-[#EFF4F6] mb-3 w-8 h-8 stroke-black stroke-[2]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M64 0C28.7 0 0 28.7 0 64L0 448c0 35.3 28.7 64 64 64l256 0c35.3 0 64-28.7 64-64l0-288-128 0c-17.7 0-32-14.3-32-32L224 0 64 0zM256 0l0 128 128 0L256 0zM216 408c0 13.3-10.7 24-24 24s-24-10.7-24-24l0-102.1-31 31c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l72-72c9.4-9.4 24.6-9.4 33.9 0l72 72c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-31-31L216 408z"/></svg><span className="mt-2 mx-2 text-center">Drag and Drop file here or <span className="underline-offset-2 underline">Choose file</span></span>
             </label>
-            <div className="flex mt-2 text-[#AFB6C2] justify-between gap-3 pb-3"><span className="">Supported formats: PDF, TXT, DOC, DOCX</span></div>
+            <div className="flex mt-2 text-[#AFB6C2] justify-between gap-3 pb-3"><span className="">Supported formats: PDF, TXT</span></div>
             <input type="file" id="upload" name="upload"  className="" accept=".pdf,.txt,.doc,.docx" onChange={handleChange} hidden />
         </form>
         </section>
